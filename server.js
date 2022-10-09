43da2f7b1a77d78fdee140f0673e5e8cd8722304
vpratnicki <vanessa@pratnicki.net>
2022-10-09 12:33:49 -0400

    front-end file setup

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 // require the data
 const { animals } = require('./data/animals');
 
@@ -49,6 +57,32 @@ function findById(id, animalsArray) {
     return result;
   }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 // add the route
 app.get('/api/animals', (req,res) => {
     let results = animals;
@@ -57,7 +91,6 @@ app.get('/api/animals', (req,res) => {
     }
     res.json(results);
 });
-
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
@@ -67,6 +100,25 @@ app.get('/api/animals/:id', (req, res) => {
     }
   });
 
+// defined a route that listens for POST requests
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+  // add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+  res.json(animal);
+  }
+});
+
+// get a route to index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
   
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
