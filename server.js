f8bf820b8169b47d0e2f94e0efe61a76c6258215
vpratnicki <vanessa@pratnicki.net>
2022-10-09 15:34:15 -0400

    Feature: front end complete

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// parse front end
+app.use(express.static('public'));
 
 // require the data
 const { animals } = require('./data/animals');
@@ -118,7 +120,17 @@ app.post('/api/animals', (req, res) => {
 // get a route to index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+// wildcard route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
   
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
